1953178045557c1eb4310a66f8b0dc817b13a69a
andy <thamandyy@gmail.com>
2018-12-11 18:42:36 -0500

    numpy

--- webtoon-crop.py
+++ webtoon-crop.py
@@ -1,3 +1,18 @@
 import os
 from PIL import Image
+import numpy as np
 
+imgOpen = Image.open('images/re1.jpg')# import image
+img = np.array(imgOpen) #convert to array
+
+#delete white/black space
+
+
+
+#save as new image
+
+
+# TODO: 
+# stitch whole image? 
+# pros - single file. cons - large size
+# scrape and download images from a source
